1d7064e5cea5f46da3f66d9823a1239130344b3d
Aabhas Bisaria <aabhasbisaria@gmail.com>
2026-02-11 22:30:51 +0530

    chocolate changes

---
+++ hug-day/hug.js
@@ -0,0 +1,17 @@
+const scene = document.getElementById("scene");
+const finalMessage = document.getElementById("finalMessage");
+
+let hugged = false;
+
+scene.addEventListener("click", () => {
+  if (!hugged) {
+    scene.classList.add("hug");
+    finalMessage.classList.remove("hidden");
+
+    setTimeout(() => {
+      finalMessage.classList.add("show");
+    }, 800);
+
+    hugged = true;
+  }
+});
